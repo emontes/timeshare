af588c124354c3e40f997c588dc27df72193be52
uhaish <uhaish@gmail.com>
2013-06-24 11:17:11 +0530

    project basic setup

--- config/autoload/global.php
+++ config/autoload/global.php
@@ -13,4 +13,22 @@
 
 return array(
     // ...
+    
+    'db' => array(
+        'driver'         => 'Pdo',
+        'username'       => 'root',
+        'password'       => 'TravelAround',
+        'dsn'            => 'mysql:dbname=eacquisi_timeshares;host=localhost',
+        'driver_options' => array(
+            PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES \'UTF8\''
+        ),
+    ),
+    'service_manager' => array(
+        'factories' => array(
+            'Zend\Db\Adapter\Adapter'
+            => 'Zend\Db\Adapter\AdapterServiceFactory',
+        ),
+    ),
+    
+    
 );
